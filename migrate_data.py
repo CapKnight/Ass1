f4c31a21c896194acd5c6572bb5b7bc5ba05449f
MUZAFFAR SALEEM <m.saleem.24@abdn.ac.uk>
2025-04-22 16:40:13 +0000

    updated migrate.py

--- migrate_data.py
+++ migrate_data.py
@@ -43,7 +43,8 @@ class Command(BaseCommand):
 
         success_count = 0
         error_count = 0
-
+        
+# error handling
         try:
             with open(file_path, encoding='utf-8') as f:
                 reader = csv.DictReader(f)
